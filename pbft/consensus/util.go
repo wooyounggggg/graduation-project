39da07297f6b2b81cd76be73d32e339c864be2fc
wooyoung <souljit2@gmail.com>
2021-05-15 02:18:27 +0900

    feat: MakeNodeTable func 구현
    실행문의 인자에 따라 노드의 총 개수(N) 및 클러스터의 개수(K)를
    동적으로 조절하도록 하는 MakeNodeTable을 구현하였습니다.

--- pbft/consensus/util.go
+++ pbft/consensus/util.go
@@ -3,6 +3,7 @@ package consensus
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"strconv"
 )
 
 func Hash(content []byte) string {
@@ -11,3 +12,23 @@ func Hash(content []byte) string {
 	return hex.EncodeToString(h.Sum(nil))
 }
 
+/** 
+	func MakeNodeTable
+	@brief
+		1~N번까지 노드의 번호를 기반으로
+		{ nodeId : 'localhost:11XX'}의 형태로 만들어줍니다.
+	@params
+		max int : 노드의 총 개수
+*/
+func MakeNodeTable(max int) map[string]string {
+    min := 1
+	nodeArray := make([]int, max-min+1)
+    for i := range nodeArray {
+        nodeArray[i] = min + i
+    }
+	nodeTable := map[string]string {}
+	for i := range nodeArray {
+		nodeTable[strconv.Itoa(i+1)] = "localhost:" + strconv.Itoa(1110 + i+1)
+	}
+    return nodeTable
+}
