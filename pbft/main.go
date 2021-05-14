38bea7736fabeb82e0a26136e830484c5e17c0fa
wooyoung <souljit2@gmail.com>
2021-05-15 02:26:57 +0900

    feat: main 함수에서 노드 개수와 클러스터 개수를 받도록 설정
    프로그램 실행시에 2번째 인자를 노드 개수(N),
    3번째 인자를 클러스터 개수(K)로 받도록 코드를 수정하였습니다
    
    사용 : go run main.go [nodeId] [N] [K]

--- pbft/main.go
+++ pbft/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"graduation-project/pbft/network"
 	"os"
+	"strconv" /* 문자열 -> 정수형 변환을 위한 module  */
 )
 
 func main() {
 	nodeID := os.Args[1]
-	server := network.NewServer(nodeID)
+	N, err := strconv.Atoi(os.Args[2]) /* 2번째 실행 인자로 전체 node의 개수 N을 받습니다. */
+	if err != nil {
+		fmt.Println("err: strconv.Atoi err")
+		os.Exit(1);
+	}
+	K, err := strconv.Atoi(os.Args[3]) /* 3번째 실행 인자로 클러스터의 개수 K를 받습니다. */
+	if err != nil {
+		fmt.Println("err: strconv.Atoi err")
+		os.Exit(1);
+	}
+	server := network.NewServer(nodeID, N, K)
 
 	server.Start()
 }
